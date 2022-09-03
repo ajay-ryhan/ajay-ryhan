df22668041172c2d487ee4d4ee86033f00c2ed66
ajay-ryhan <ajay.ryhan@gmail.com>
2022-09-03 20:04:11 +0530

    First programming

---
+++ src/UdemyLearning/MyFirstProgramming.java
@@ -0,0 +1,14 @@
+package UdemyLearning;
+
+public class MyFirstProgramming {
+
+    public static void main (String[] args) {
+        System.out.println("Hello, world!");
+
+        //Declaring variables
+        int numberOne;
+        numberOne = 4;
+        String variableString = "Ajay sri balaji";
+        System.out.println(variableString + "  " + numberOne);
+    }
+}
